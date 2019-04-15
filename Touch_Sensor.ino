6ac1744f5167ee614e38d54bb3b3705bfb7f2429
sljj987 <49646387+sljj987@users.noreply.github.com>
2019-04-15 21:14:16 +0800

    Add files via upload

---
+++ Touch_Sensor.ino
@@ -0,0 +1,24 @@
+int touch1 = 2;
+int touch2 = 3; 
+int out = 13;  
+int state = HIGH;  
+int r;           
+int p = LOW;    
+long time = 0;       
+long debounce = 200;   
+void setup()
+{
+  pinMode(touch1, INPUT);
+  pinMode(touch2, INPUT);
+  pinMode(out, OUTPUT);
+}
+void loop()
+{
+  r = digitalRead(touch1)&&digitalRead(touch2);
+  if (r == HIGH )
+    delay(2000);
+    if (r == HIGH )
+      digitalWrite(out, HIGH);
+    else 
+      digitalWrite(out, LOW);
+}
